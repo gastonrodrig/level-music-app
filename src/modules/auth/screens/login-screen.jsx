8a17400aa51efd2b462fdb2ba96d59c4b16a2c4e
Gaston Rodriguez <gaston.rodriguez@icloud.com>
2025-11-06 19:58:51 -0500

    Modificara pantalla de inicio

--- src/modules/auth/screens/login-screen.jsx
+++ src/modules/auth/screens/login-screen.jsx
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export const LoginScreen = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Hola, ahora sí funciono.</Text>
     </View>
   )
